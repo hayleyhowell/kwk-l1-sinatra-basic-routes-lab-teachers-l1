52a62cdd07ca563312748ddad3d6de260922c174
Hayley Howell <hayley_howell@sbcglobal.net>
2018-06-25 20:21:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,15 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get 'name' do
+  get '/name' do
     "My name is Hayley"
   end
   
-  get 'hometown' do
+  get '/hometown' do
     "My hometown is Los Altos"
   end
   
-  get 'favorite-song' do
+  get '/favorite-song' do
     "My favorite song is anything by Taylor Swift"
   end
   
